6a3c6dd0f44cb4dcb436e770116478cf4749484b
Nathan Lambert <nathanl@allenai.org>
2024-06-26 22:59:03 +0000

    fix

--- demo_tools/interface.py
+++ demo_tools/interface.py
@@ -202,7 +202,7 @@ class EnhancedChatInterface(Blocks):
                 with Column(scale=3):
                     if title:
                         title_str = f"""<div style="display: flex; align-items: center;">
-                                        <img src="/file=assets/ai2-logo.png" style="width: 25px; height: 25px; margin-top: 10px; margin-bottom: -7px; margin-right: 5px;">
+                                        <img src="/file=demo_tools/assets/ai2-logo.png" style="width: 25px; height: 25px; margin-top: 10px; margin-bottom: -7px; margin-right: 5px;">
                                         <h1 style="margin-bottom: 1rem;">{self.title}</h1>
                                     </div>"""
                         # Markdown(f"<h1 style='margin-bottom: 1rem'>{self.title}</h1>")  # removed text-align: center;
